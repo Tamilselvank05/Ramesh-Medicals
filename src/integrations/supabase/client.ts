d8609af8cc16690ea8bdfd9e8443ec1200efa97f
TAMILSELVAN K <168197303+Tamilselvank05@users.noreply.github.com>
2025-05-18 10:50:19 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://qwxdoxiaeyedktowdmyt.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eGRveGlhZXllZGt0b3dkbXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MjU0NDMsImV4cCI6MjA1OTUwMTQ0M30.a2XL_0dVkOxImLllwVd_zE__UFGbI8N2-3A9UnTcqU0";
+const SUPABASE_URL =process.env.REACT_APP_SUPABASE_URL!;    // "https://qwxdoxiaeyedktowdmyt.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY!;//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3eGRveGlhZXllZGt0b3dkbXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5MjU0NDMsImV4cCI6MjA1OTUwMTQ0M30.a2XL_0dVkOxImLllwVd_zE__UFGbI8N2-3A9UnTcqU0";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
